4dd1a4c51329379ab94874c43c8c0610aa843d3f
Radoslaw Choluj <radekcholuj@wp.pl>
2020-12-05 22:04:22 +0100

    konwersja typow

---
+++ App.java
@@ -0,0 +1,20 @@
+class App{
+	public static void main(String args[]){
+	// zadanie 1
+	int employees = 10420;
+	byte firstYearEmployees = 10;
+	
+	
+	employees = firstYearEmployees;
+	System.out.println("Employees :"+employees);
+	
+	
+	// zadanie 2
+	int zakopane;
+	short number = 27266;
+	
+	zakopane = number;
+	System.out.println("Zakopane :"+zakopane);
+	
+	}
+}
